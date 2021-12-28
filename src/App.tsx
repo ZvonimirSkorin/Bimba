b7c8af481713b532d918a076e3200215fcb32866
Zvonimir Skorin <skorinzvonimir0@gmail.com>
2021-12-29 00:25:51 +0100

    marsel work

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,23 @@ import logo from "./logo.svg";
 import "./App.css";
 import "./style/app.scss";
 import { Kalendar } from "./Components/Kalendar";
+import { Services } from "./Components/AddService";
 
 function App() {
   return (
     <div className="App">
       <a href="/zvone">kalendar</a>
+
+      <a style={{ display: "block" }} href="/marsel">
+        usluge
+      </a>
       <Router>
         <Routes>
           <Route>
             <Route path="/zvone" element={<Kalendar />} />
+          </Route>
+          <Route>
+            <Route path="/marsel" element={<Services />} />
           </Route>
         </Routes>
       </Router>
